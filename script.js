b8c3fe0cc4330faae92dd56f6b45811cfde5d29c
Priyansh.Tripathi <priyansh.tripathi@nirvanasolutions.com>
2023-01-24 17:59:45 +0530

    intial commit

--- script.js
+++ script.js
@@ -1,94 +1,94 @@
-if(localStorage.key(0)==null){
-  localStorage.setItem("new",JSON.stringify([]));
-  localStorage.setItem("development",JSON.stringify([]));
-  localStorage.setItem("qa", JSON.stringify([]));
-  localStorage.setItem("finished",JSON.stringify([]));
-  }
-  $(document).ready(function(){
-      createNewCard();
-      $(window).scroll(function(){
-          if(this.scrollY > 20){
-              $('.navbar').addClass("sticky");
-          }else{
-              $('.navbar').removeClass("sticky");
-          }
-      });}); 
-  let mybutton = document.getElementById("scroll-to-top");
-  window.onscroll = function() {scrollFunction()};
-  function scrollFunction() {
-    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
-      mybutton.style.display = "block";
+let count = 1;
+$(document).ready(function () {
+  createNewCard();
+  $(window).scroll(function () {
+    if (this.scrollY > 20) {
+      $(".navbar").addClass("sticky");
     } else {
-      mybutton.style.display = "none";
+      $(".navbar").removeClass("sticky");
     }
+  });
+});
+let mybutton = document.getElementById("scroll-to-top");
+window.onscroll = function () {
+  scrollFunction();
+};
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 300 ||
+    document.documentElement.scrollTop > 300
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
   }
-  function topScroll() {
-    document.documentElement.scrollTop = 0;
-  }
-  $("#add").click(
-    function(event){
-      event.preventDefault();
-      let r ="id" + Math.random().toString(16).slice(2);
-      let issue=$('#issue').val();
-      let assigned=$('#assigned-to').val();
-      let desc=$('#exampleFormControlTextarea1').val();
-      let sev=$('#sev').find(":selected").val();
-      let card={
-        "hash":r,
-        "issue":issue,
-        "assigned":assigned,
-        "desc":desc,
-        "sev":sev
-      };
-      addNew(card);
-    }
-  );
-  let hash;
-  function createNewCard(){
-  let newOut=JSON.parse(localStorage.getItem('new'));
-  $('.cardAdd').empty();
-  $('.card-add').empty();
-  for(var x of newOut){
-    hash=x['hash'];
-    let temp=`<br><div class="card">
-    <div class="card-body">
-      <p class="card-title text-start">Issue Id: ${x['hash']}</p>
-      <p class="alert alert-primary">OPEN</p>
-      <h5 class="card-title alert alert-danger">${x['issue']}</h5>
-      <h6 class="card-subtitle mb-2 text-muted">Assigned To <br> ${x['assigned']}</h6>
-      <p class="card-text alert alert-dark">description: <br>${x['desc']}</p>
-      <h5 class="card-text">Severity: ${x['sev']}</h5>
-      <button type="button" class="btn btn-danger" onclick="deleteIssue(hash)">Delete</button>
-      <button type="button" class="btn btn-primary" >Next</button></div>
-  </div><br>`;
-  $('.cardAdd').prepend(temp);
-  $('.card-add').prepend(temp);
-  }
+}
+function topScroll() {
+  document.documentElement.scrollTop = 0;
+}
+$("#add").click(function (event) {
+  event.preventDefault();
+  let hash = "id" + Math.random().toString(16).slice(2);
+  let issue = $("#issue").val();
+  let assigned = $("#assigned-to").val();
+  let desc = $("#exampleFormControlTextarea1").val();
+  let sev = $("#sev").find(":selected").val();
+  let card = {
+    'hash': hash,
+    'issue': issue,
+    'assigned': assigned,
+    'desc': desc,
+    'sev': sev,
+    'state': "Open",
   };
-  function deleteIssue(hash){
-    let newIssue=localStorage.getItem('new');
-    console.log(newIssue+" "+typeof(newIssue));
-    // for(let i =0;i<newIssue.length;i++){
-    //   if(newIssue[i]['hash']==hash){
-    //     break;
-    //   }
-    // // }
-    // newArray=newArray(newIssue);
-    newIssue.split(',');
-  
-    console.log(JSON.parse(newIssue)+typeof(newIssue))
+  addNew(hash,card);
+});
+
+function createNewCard() {
+  if(localStorage.key(0)==null){
+    return;
   }
-  function newArray(newIssue){
-     
+  $(".newCardAdd").empty();
+  $(".card-add").empty();
+  for (let i=0;i< localStorage.length;i++) {
+    let details=JSON.parse(localStorage.getItem(localStorage.key(i)));
+    let btn_prev=details['state']=='Open'? "Delete": "Previous";
+    let btn_next=details['state']=='Finished'? "Delete": "Next";
+    let temp = `<br><div class="card">
+    <div class="card-body">
+      <p class="card-title text-start">Issue Id: ${details['hash']}</p>
+      <p class="alert alert-primary">${details['state']}</p>
+      <h5 class="card-title alert alert-danger">${details["issue"]}</h5>
+      <h6 class="card-subtitle mb-2 text-muted">Assigned To <br> ${details["assigned"]}</h6>
+      <p class="card-text alert alert-dark">description: <br>${details["desc"]}</p>
+      <h5 class="card-text">Severity: ${details["sev"]}</h5>
+      <button type="button" class="btn btn-danger" onclick="deleteIssue()">${btn_prev}</button>
+      <button type="button" class="btn btn-primary" onclick="nextIssue()" >${btn_next}</button></div>
+  </div><br>`;
+  $(".card-add").prepend(temp);
+    if(details['state']=="Open"){
+    $(".newCardAdd").prepend(temp);
+    }
+    else if(details['state']=="development"){
+      $(".devCardAdd").prepend(temp);
+    }
+    else if(details['state']=="QA"){
+      $(".qaCardAdd").prepend(temp);
+    }
+    else if(details['state']=='Finished'){
+      $(".finCardAdd").prepend(temp);
+    }
   }
-  // function nextIssue(){
-  
-  // }
-  function addNew(card){
-    let item=localStorage.getItem('new');
-    item=JSON.parse(item);
-    item.push(card);
-    localStorage.setItem('new',JSON.stringify(item));
-    console.log(localStorage.getItem('new'));
-    createNewCard();
-  }
+}
+function deleteIssue(hash){
+  console.log(hash);
+  // localStorage.removeItem(hash);
+  $(".newCardAdd").empty();
+  $(".card-add").empty();
+  createNewCard();
+}
+function addNew(hash,card) {
+  localStorage.setItem(hash, JSON.stringify(card));
+  console.log(card+" "+"done");
+  createNewCard();
+}
